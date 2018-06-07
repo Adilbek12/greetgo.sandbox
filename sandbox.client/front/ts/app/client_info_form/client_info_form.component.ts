6b6b6ed8ce107af8fe4dabe980aa0a2c987c0200
Adilbek <mv.adilbek@gmail.com>
2018-06-07 16:06:28 +0600

    Fix validation

--- sandbox.client/front/ts/app/client_info_form/client_info_form.component.ts
+++ sandbox.client/front/ts/app/client_info_form/client_info_form.component.ts
@@ -1,7 +1,8 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, QueryList, ViewChildren} from "@angular/core";
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from "@angular/core";
 import {HttpService} from "../HttpService";
 import {ClientToSave} from "../../model/ClientToSave";
 import {Charm} from "../../model/Charm";
+import {Gender} from "../../model/Gender";
 
 @Component({
   selector: 'client-info-form-component',
@@ -12,7 +13,10 @@ export class ClientInfoFormComponent implements OnInit{
   @Input() clientId: number;
   @Output() onClose = new EventEmitter<boolean>();
 
-  @ViewChildren('valid_input') valid_input: QueryList<ElementRef>;
+  @ViewChild('birthDayInput') birthDayInput: ElementRef;
+
+  title = "Новый клиент";
+  buttonTitle = "Добавить";
 
   charms: Array<Charm> = new Array<Charm>();
   wrongMessageEnable: boolean = false;
@@ -20,19 +24,25 @@ export class ClientInfoFormComponent implements OnInit{
 
   constructor(private httpService: HttpService) {
     this.loadCharms();
+
   }
 
   ngOnInit(): void {
     if (this.clientId != null) {
+      this.title = "Изменить данные клиента";
+      this.buttonTitle = "Изменить";
       let clientId = this.clientId as number;
       this.httpService.get("/client/detail", {"clientId": clientId}).toPromise().then(result => {
         this.clientToSave = ClientToSave.copy(result.json());
         console.log(this.clientToSave);
       })
     }
-
-    else
+    else {
       this.clientToSave = new ClientToSave();
+      this.clientToSave.gender = Gender.MALE;
+      this.clientToSave.charmId = 1;
+    }
+    this.setMaxDate();
   }
 
   loadCharms() {
@@ -44,10 +54,17 @@ export class ClientInfoFormComponent implements OnInit{
   }
 
   checkClient(): boolean {
+    let re1 = /^[a-zA-Z_]*$/;
+    let re2 = /^[a-zA-Z0-9_]*$/;
     return this.checkData(this.clientToSave.addressReg) &&
+      this.clientToSave.addressReg.house.match(re2) &&
+      this.clientToSave.addressReg.flat.match(re2) &&
+      this.clientToSave.addressReg.street.match(re2) &&
       this.checkClientPhones() &&
       this.checkData(this.clientToSave.name) &&
+      this.clientToSave.name.match(re1) != null &&
       this.checkData(this.clientToSave.surname) &&
+      this.clientToSave.surname.match(re1) != null &&
       this.checkData(this.clientToSave.birth_day) &&
       this.checkData(this.clientToSave.gender) &&
       this.checkClientPhones();
@@ -58,15 +75,19 @@ export class ClientInfoFormComponent implements OnInit{
   }
 
   checkClientPhones(): boolean {
+    let re = /^[0-9]{8}[0-9]*$/;
     return this.checkData(this.clientToSave.mobilePhone.number) &&
+      this.clientToSave.mobilePhone.number.match(re)!=null &&
       this.checkData(this.clientToSave.workPhone.number) &&
-      this.checkData(this.clientToSave.homePhone.number);
+      this.clientToSave.workPhone.number.match(re)!=null &&
+      this.checkData(this.clientToSave.homePhone.number) &&
+      this.clientToSave.homePhone.number.match(re)!=null;
   }
 
   saveButtonClicked () {
     if (this.checkClient()) {
       this.wrongMessageEnable = false;
-      //this.saveClient();
+      this.saveClient();
     }
     else {
       this.wrongMessageEnable = true;
@@ -74,7 +95,6 @@ export class ClientInfoFormComponent implements OnInit{
   }
 
   saveClient() {
-    console.log("CLIENT TO SAVE="+this.clientToSave);
     this.httpService.post("/client/save", {
       "clientToSave": JSON.stringify(this.clientToSave)
     }).toPromise().then(result => {
@@ -88,5 +108,20 @@ export class ClientInfoFormComponent implements OnInit{
 
   setBDate(dateText) {
     this.clientToSave.birth_day = new Date(dateText);
+  }
+
+  setMaxDate() {
+    let today = new Date();
+    let dd:string = today.getDate().toString();
+    let mm:string = (today.getMonth()+1).toString(); //January is 0!
+    let yyyy: string = today.getFullYear().toString();
+    if(dd.length<10){
+      dd='0'+dd
+    }
+    if(mm.length<10){
+      mm='0'+mm
+    }
+
+    this.birthDayInput.nativeElement.setAttribute("max", yyyy+'-'+mm+'-'+dd);
   }
 }
